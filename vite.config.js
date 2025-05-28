b551ce3c850750bd6e7480ab06a5e39f6f6e4b0d
Prasanti <prasantihikmatullisan@gmail.com>
2025-05-28 18:51:53 +0700

    update base config

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,11 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/profile",
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "./src")
+
     }
   }
 })
